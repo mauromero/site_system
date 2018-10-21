786f141927e9bfc4338c46bd4d2ba82b3f312144
Mauricio Romero <mromeroval@ucdavis.edu>
2018-10-21 00:20:13 -0700

    Added middleware to check for active users

---
+++ app/Http/Middleware/CheckActive.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckActive
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (auth()->user() && auth()->user()->active!=1) {
+            $response = redirect('login');
+        }
+        return $response;
+        
+    }
+}
